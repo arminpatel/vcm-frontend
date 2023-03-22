4a2dace41326a9a0241fba2516c523511652d989
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2023-03-23 01:43:57 +0530

    add basic functionality to home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Navbar } from "../components/Navbar";
 import Title from "../components/Title";
+import Subtitle from "../components/Subtitle";
+import HeroButtonGroup from "../components/HeroButtonGroup";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Title />
+      <Subtitle />
+      <HeroButtonGroup />
     </>
   );
 };
