4838c19bd2d64ede6327c901a04376d37671863e
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2023-03-26 16:42:58 +0530

    add contest page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CreateContest from "./pages/CreateContest";
+import Contest from "./pages/Contest";
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/create-contest" element={<CreateContest />} />
+      <Route path="/contest" element={<Contest />} />
     </Routes>
   );
 }
