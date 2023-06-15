c729150a7ff9b83a182175f9ab5e8a93c4a5e749
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2023-06-15 12:03:15 +0530

    make profile page functional (#38)

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,118 +1,164 @@
 import { Navbar } from "../components/Navbar";
-import {useState} from 'react'
+import { useState } from "react";
 import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import { makeStyles } from "@mui/styles";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const useStyles = makeStyles(() => ({
   header: {
-    width: '40vw',
-    backgroundColor: '#D4F1F4',
-    borderRadius: '1rem',
-    padding: '0 1rem 0.5rem 1rem',
-    marginTop: '1rem',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    width: "40vw",
+    backgroundColor: "#D4F1F4",
+    borderRadius: "1rem",
+    padding: "0 1rem 0.5rem 1rem",
+    marginTop: "1rem",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   overview: {
-    width: '40vw',
-    backgroundColor: '#D4F1F4',
-    borderRadius: '1rem',
-    padding: '1rem 1rem 0.5rem 1rem',
-    marginTop: '1.5rem',
+    width: "40vw",
+    backgroundColor: "#D4F1F4",
+    borderRadius: "1rem",
+    padding: "1rem 1rem 0.5rem 1rem",
+    marginTop: "1.5rem",
   },
   contest: {
-    width: '40vw',
-    backgroundColor: '#D4F1F4',
-    borderRadius: '1rem',
-    padding: '1rem 1rem 0.5rem 1rem',
-    marginTop: '1.5rem',
+    width: "40vw",
+    backgroundColor: "#D4F1F4",
+    borderRadius: "1rem",
+    padding: "1rem 1rem 0.5rem 1rem",
+    marginTop: "1.5rem",
   },
-}))
+}));
 
-const data = {
-  username: '0xarmin',
-  name: 'Armin Patel',
-  cf_id: '0xarmin',
-  cc_id: 'ccoder5',
-  atc_id: 'arminp',
-  constests: [
-    {
-      name: 'Codeforces Round #744 (Div. 3)',
-      start: '2021-10-10T07:35:00+05:30',
-      duration: 120,
-    },
-    {
-      name: 'Codeforces Round #743 (Div. 2)',
-      start: '2021-10-09T07:35:00+05:30',
-      duration: 120,
-    },
-    {
-      name: 'Codeforces Round #742 (Div. 2)',
-      start: '2021-10-08T07:35:00+05:30',
-      duration: 120,
-    },
-    {
-      name: 'Codeforces Round #741 (Div. 2)',
-      start: '2021-10-07T07:35:00+05:30',
-      duration: 120,
-    },
-    {
-      name: 'Codeforces Round #740 (Div. 2)',
-      start: '2021-10-06T07:35:00+05:30',
-      duration: 120,
-    },
-  ]
-}
 export function Profile() {
-  var [tabValue, setTabValue] = useState("overview")
-  const classes = useStyles()
+  const [tabValue, setTabValue] = useState("overview");
+  const { username } = useParams();
+  const classes = useStyles();
+
+  const { status, data, error } = useQuery(["profile"], async () => {
+    let res = await axios.get(`api/users/${username}`);
+    let dataDetails = res.data;
+    let dataContests;
+    try {
+      let res2 = await axios.get(`api/contests/${username}`);
+      dataContests = res2.data;
+    } catch (err) {
+      dataContests = undefined;
+    }
+    return { dataDetails, dataContests };
+  });
+
+  if (status === "loading") {
+    return (
+      <>
+        <span>loading</span>
+      </>
+    );
+  }
+  if (error) {
+    return (
+      <>
+        <span> Some Error Occured </span>
+      </>
+    );
+  }
+
+  const { dataDetails, dataContests } = data;
+
   return (
     <>
       <Navbar />
       <div className={classes.header}>
         <div>
-          <Typography variant="h4">{data.username}</Typography>
-          <Typography>{data.name}</Typography>
+          <Typography variant="h4">{dataDetails.username}</Typography>
+          <Typography>
+            {dataDetails.first_name + " " + dataDetails.last_name}
+          </Typography>
         </div>
         <div>
-          <Button variant="contained">Edit Profile</Button> 
+          <Button variant="contained">Edit Profile</Button>
         </div>
       </div>
 
-      <Tabs value={tabValue} onChange={(e, val)=> {setTabValue(val)}}>
-        <Tab value = "overview" label="Overview" />
-        <Tab value = "contests" label="Contests" />
+      <Tabs
+        value={tabValue}
+        onChange={(e, val) => {
+          setTabValue(val);
+        }}
+      >
+        <Tab value="overview" label="Overview" />
+        <Tab value="contests" label="Contests" />
       </Tabs>
 
       {tabValue === "overview" && (
         <div className={classes.overview}>
-          <Typography>Codeforces: <Link href={`https://codeforces.com/profile/${data.cf_id}`}> {data.cf_id} </Link> </Typography>
-          <Typography>Atcoder: <Link href={`https://atcoder.jp/users/${data.atc_id}`}>{data.atc_id} </Link> </Typography>
-          <Typography>CodeChef: <Link href={`https://codechef.com/users/${data.cc_id}`}> {data.cc_id} </Link> </Typography>
+          <Typography>
+            Codeforces:{" "}
+            {dataDetails.profile.cf_handle ? (
+              <Link
+                href={`https://codeforces.com/profile/${dataDetails.profile.cf_handle}`}
+              >
+                {" "}
+                {dataDetails.profile.cf_handle}{" "}
+              </Link>
+            ) : (
+              "Unavailable"
+            )}
+          </Typography>
+          <Typography>
+            Codechef:{" "}
+            {dataDetails.profile.cc_handle ? (
+              <Link
+                href={`https://codechef.com/users/${dataDetails.profile.cc_handle}`}
+              >
+                {" "}
+                {dataDetails.profile.cc_handle}{" "}
+              </Link>
+            ) : (
+              "Unavailable"
+            )}
+          </Typography>
+          <Typography>
+            AtCoder:{" "}
+            {dataDetails.profile.ac_handle ? (
+              <Link
+                href={`https://atcoder.jp/users/${dataDetails.profile.ac_handle}`}
+              >
+                {" "}
+                {dataDetails.profile.ac_handle}{" "}
+              </Link>
+            ) : (
+              "Unavailable"
+            )}
+          </Typography>
         </div>
       )}
 
       {tabValue === "contests" && (
         <div className={classes.contestsContainer}>
-          {data.constests.map((contest, index) => {
-          const dt = new Date(Date.parse(contest.start))
-          return (
-            <div key={index} className={classes.contest}>
-              <Typography>{contest.name}</Typography>
-              <div>
-                <Typography> Start Time: {dt.toString()}</Typography>
-                <Typography>Duration: {contest.duration}</Typography>
-              </div>
-            </div>
-          )}
-        )}
+          {dataContests
+            ? dataContests.map((contest, index) => {
+                const dt = new Date(Date.parse(contest.start));
+                return (
+                  <div key={index} className={classes.contest}>
+                    <Typography>{contest.name}</Typography>
+                    <div>
+                      <Typography> Start Time: {dt.toString()}</Typography>
+                      <Typography>Duration: {contest.duration}</Typography>
+                    </div>
+                  </div>
+                );
+              })
+            : "No Contest to Show"}
         </div>
-        )}
+      )}
     </>
-  )
+  );
 }
