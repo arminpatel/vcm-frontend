c01ea4f869557869d3c55f89c5a71c4d01043cdc
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2023-06-25 20:17:03 +0530

    setup daisy and change navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,57 +1,17 @@
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import { makeStyles } from "@mui/styles";
 import { Link } from "react-router-dom";
 
-const useStyles = makeStyles(() => ({
-  title: {
-    flexGrow: 1,
-  },
-}));
-
-const menuData = [
-  {
-    label: "Home",
-    href: "/",
-  },
-  {
-    label: "Create Contest",
-    href: "/create-contest",
-  },
-  {
-    label: "Login",
-    href: "/login",
-  },
-];
-
 export function Navbar() {
-  const classes = useStyles();
-
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" className={classes.title}>
-          {" "}
-          Virtual Contest Maker{" "}
-        </Typography>
-        {menuData.map(({ label, href }) => {
-          return (
-            <Button
-              key={label}
-              {...{
-                key: label,
-                color: "inherit",
-                component: Link,
-                to: href,
-              }}
-            >
-              {label}
-            </Button>
-          );
-        })}
-      </Toolbar>
-    </AppBar>
-  );
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a href="/" className="btn btn-ghost normal-case text-xl"> Virtual Contest Maker </a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-vertical lg:menu-horizontal px-1">
+          <li><Link to="/create-contest"> Create Contest </Link></li>
+          <li><Link to="/login"> Sign In </Link></li>
+        </ul>
+      </div>
+    </div>
+  )
 }
