00cc33b1e2861a390ddeafc651caa98853aa07a4
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2023-07-18 00:00:33 +0530

    add standard error pages (#54)

---
+++ src/pages/errors/Unauthorized401.jsx
@@ -0,0 +1,33 @@
+import React from "react"
+import { Navbar } from "../../components/Navbar"
+import { ReactComponent as ErrorSvg } from "../../../assets/errorpageillustration.svg"
+import Footer from "../../components/Footer"
+import { useNavigate } from "react-router-dom"
+
+
+const Unauthorized401 = () => {
+    const navigate = useNavigate();
+
+    return (
+        <>
+        <Navbar />
+        <div className="mt-20 w-screen h-screen flex flex-col items-center"> 
+           <ErrorSvg className="w-2/5 h-2/5"/> 
+           <h1 className="text-8xl"> 401 </h1> 
+           <h2 className="text-2xl"> You do not have the required permission to view this page </h2>
+           <div className="mt-10">
+            <button
+              className="shadow-md py-3 px-6 text-white rounded-md bg-red-500 hover:bg-red-600"
+              onClick={() => navigate("/")}
+            >
+                Go Back Home
+            </button>
+           </div> 
+        </div> 
+        <Footer />
+        </>
+     )
+}
+
+export default Unauthorized401;
+
