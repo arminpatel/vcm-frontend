b0f58acc8877d5b112161a0149a619835f394940
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2023-08-14 14:16:44 +0530

    update prod api endpoint

--- src/utils/envConfig.js
+++ src/utils/envConfig.js
@@ -1,5 +1,5 @@
 const config = {
-  apiUrl : import.meta.env.PROD ? 'https://arminpatel.pythonanywhere.com' : 'http://127.0.0.1:8000',
+  apiUrl : import.meta.env.PROD ? 'https://vcm-backend-production.up.railway.app' : 'http://127.0.0.1:8000',
 }
 
 export default config
