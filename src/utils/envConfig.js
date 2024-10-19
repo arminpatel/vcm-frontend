a45cde7933fd51cdb7ea06511773c33c8ceba143
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2024-10-19 15:03:12 +0530

    add github workflow for continuous deployment

--- src/utils/envConfig.js
+++ src/utils/envConfig.js
@@ -1,7 +1,5 @@
 const config = {
-  apiUrl: import.meta.env.PROD
-    ? "https://vcm-backend-production.up.railway.app"
-    : "http://127.0.0.1:8000",
+  apiUrl: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000"
 };
 
 export default config;
