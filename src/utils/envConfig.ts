fa12c2d59cc56fb091e85abb3a06ba87fea28584
arminpatel <72291325+arminpatel@users.noreply.github.com>
2024-11-10 19:20:39 +0530

    Update envConfig.ts

--- src/utils/envConfig.ts
+++ src/utils/envConfig.ts
@@ -1,11 +1,5 @@
-interface Config {
-  apiUrl: string;
-}
-
-const config: Config = {
-  apiUrl: import.meta.env.PROD
-    ? "https://vcm-backend-production.up.railway.app"
-    : "http://127.0.0.1:8000",
+const config = {
+  apiUrl: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000"
 };
 
 export default config;
